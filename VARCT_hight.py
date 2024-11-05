8412ead12ff3ad1b446d015257d9e904c7d4fda1
Ivan_ <117020822+Ivan00700@users.noreply.github.com>
2024-11-05 22:14:18 +0300

    Update VARCT_hight.py

--- VARCT_hight.py
+++ VARCT_hight.py
@@ -2,35 +2,39 @@ import krpc
 import matplotlib.pyplot as plt
 import time as t
 
-# Подключение к серверу kRPC
-conn = krpc.connect('altitude_check')
 
-# Получение объекта космического корабля
-vessel = conn.space_center.active_vessel
+def main():
+    # Подключение к серверу kRPC
+    conn = krpc.connect('hight_checker')
+    vessel = conn.space_center.active_vessel
 
-# Создание массивов для данных о времени и высоте
-time_values = []
-altitude_values = []
-time = 0
+    # Создание массивов для данных о времени и высоте
+    time_values = []
+    altitude_values = []
+    time = 0
 
-# Получение высоты корабля на протяжении полета
-while True:
-    # Получение текущей высоты
-    altitude = vessel.flight().surface_altitude
-    time_values.append(time)  # Запись текущего времени в массив
-    altitude_values.append(altitude)  # Запись текущей высоты в массив
+    # Получение высоты корабля на протяжении полета
+    while True:
+        altitude = vessel.flight().surface_altitude
+        time_values.append(time)  # Запись текущего времени в массив
+        altitude_values.append(altitude)  # Запись текущей высоты в массив
 
-    # Вывод считываемых данных в консоль
-    print("Время: {}, Высота: {} м".format(time, altitude))
-    t.sleep(1)
-    time += 1
-    # Проверка условия завершения сбора данных
-    if altitude > 100000:  # Остановка считывания данных при наборе высоты 100км
-        break
+        # Вывод считываемых данных в консоль
+        print("Время: {}, Высота: {} м".format(time, altitude))
+        t.sleep(1)  # КД 1 секунда для умеренных данных
+        time += 1
+        # Проверка условия завершения сбора данных
+        if altitude > 100000:  # Остановка считывания данных при наборе высоты 100км
+            # 100км тк это идеальный апогей для перехода к другим планетам Кербальской Солнечной системы
+            break
 
-    # Построение графика скорости от времени
-plt.plot(time_values, altitude_values)
-plt.title('Зависимость высоты от времени')
-plt.xlabel('Время, s')
-plt.ylabel('Высота, m')
-plt.show()
+    # Построение графика высоты от времени с помощью бибилиотеки pyplot
+    plt.plot(time_values, altitude_values)
+    plt.title('Зависимость высоты от времени')
+    plt.xlabel('Время, s')
+    plt.ylabel('Высота, m')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
